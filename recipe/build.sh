e1aae5947062966b8b2d6657d006686142a6aac6
Mark Harfouche <mark.harfouche@gmail.com>
2025-05-05 09:15:48 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,27 +6,17 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./lang/sql/jdbc
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./lang/sql/sqlite
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./lang/sql/sqlite/autoconf
 
-if [[ $(uname) == Darwin ]]; then
-  export CC=clang
-  export CXX=clang++
-  export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -headerpad_max_install_names $LDFLAGS"
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-  export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
-fi
-
+# docdir specified to avoid installation of 100MB of extracted docs
 cd build_unix
 ../dist/configure --prefix=$PREFIX \
+                  --docdir=${SRC_DIR}/do_not_install_the_docs/docs \
                   --enable-shared \
                   --disable-static \
                   --enable-cxx \
                   --enable-stl
 
 make -j$CPU_COUNT
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check -j$CPU_COUNT
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    make check -j$CPU_COUNT
 fi
 make install -j$CPU_COUNT
-
-cd $PREFIX
-find . -type f -name "*.la" -exec rm -rf '{}' \; -print
